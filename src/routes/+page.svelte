1fbe26ce5eb7226298295a1bfc0a06509546afcf
edhlz <eduh0597@gmail.com>
2025-04-15 16:45:09 -0300

    adicionei o terminal na tela

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex align-center justify-center h-screen p-12">
+    <div class="w-full h-full bg-[#1f1e1e] shadow-xl flex flex-col">
+        <!-- barra de cima do terminal deus me ajude -->
+        <div class="h-12 bg-[#1f1e1e] flex items-center px-4 border-b border-[#363636]">
+            <!-- botoes foda -->
+            <div class="flex space-x-2">
+                <div class="w-3 h-3 rounded-full bg-[#ff5f56] hover:bg-[#ff3b30] cursor-pointer"></div>
+                <div class="w-3 h-3 rounded-full bg-[#ffbd2e] hover:bg-[#ffa500] cursor-pointer"></div>
+                <div class="w-3 h-3 rounded-full bg-[#27c93f] hover:bg-[#1d9730] cursor-pointer"></div>
+            </div>
+            <!-- root user -->
+            <div class="flex-1 text-center text-gray-400 text-sm font-mono">
+                user@localhost: ~
+            </div>
+        </div>
+        
+        <!-- area de conteudo -->
+        <div class="flex-1 text-white">graças a deus isso aqui funciona</div>
+    </div>
+</div>
